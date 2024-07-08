0fbd5af353df95857b9be4241144edcd9d37a7a2
glazernir <glazer.nir@campus.technion.ac.il>
2024-07-08 21:43:20 +0300

    initial

---
+++ compi_hw3/SymboTable.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Nir on 7/8/2024.
+//
+
+#include "SymboTable.h"
---
+++ compi_hw3/SymboTable.h
@@ -0,0 +1,14 @@
+//
+// Created by Nir on 7/8/2024.
+//
+
+#ifndef COMPI_HW3_SYMBOTABLE_H
+#define COMPI_HW3_SYMBOTABLE_H
+
+
+class SymboTable {
+
+};
+
+
+#endif //COMPI_HW3_SYMBOTABLE_H
