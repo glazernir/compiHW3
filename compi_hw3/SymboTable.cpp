1591d1566fdfe325f4a1caa19a6656fa1c4591d2
glazernir <glazer.nir@campus.technion.ac.il>
2024-07-14 13:14:14 +0300

    initial

--- compi_hw3/SymboTable.cpp
+++ compi_hw3/SymboTable.cpp
@@ -7,10 +7,51 @@
 
 
 
-
-
-
-
-
-
 //=================================================================== up side is types and down side is symboltable
+
+//================ Symbol methods =============================
+
+Symbol::Symbol(std::string name, std::string type, int offset, bool is_symbol_function) {
+    this->Name = name;
+    this->Type = type;
+    this->Offset = offset;
+    this->isFunctionSymbol = is_symbol_function;
+
+}
+
+
+//================ Symbol Table methods =============================
+
+void SymbolTable::InsertSymbol(const Symbol &symbol) {
+    Symbol* s = new Symbol(symbol);
+    this->Table.push_back(s);
+    if(s->Offset >= 0){
+        this->last_var_offset = s->Offset;
+    }
+}
+
+bool SymbolTable::isSymbolInTable(const std::string &symbol_name) {
+
+    for(Symbol* s: this->Table){
+        if(s){
+            if(s->Name == symbol_name){
+                return true;
+            }
+        }
+    }
+}
+
+
+//================ tables stack methods =============================
+
+
+
+
+
+
+
+
+
+
+
+
--- compi_hw3/SymboTable.h
+++ compi_hw3/SymboTable.h
@@ -5,14 +5,64 @@
 #ifndef COMPI_HW3_SYMBOTABLE_H
 #define COMPI_HW3_SYMBOTABLE_H
 
+#include "vector"
+#include "string"
 
 
 
+//================================================================= up side is types and down side is symboltable + stack
+//of symbol tables==========================
 
-//================================================================= up side is types and down side is symboltable
-class SymbolTable {
+class Symbol{
+public:
+    std::string Name;
+    std::string Type;
+    int Offset;
+    bool isFunctionSymbol;
+    std::vector<std::string> functionParams;
+
+
+    Symbol(std::string name, std::string type,int offset,bool is_symbol_function,std::vector<std::string> functions_params);
+    ~Symbol() = default;
 
 };
+
+
+class SymbolTable {
+public:
+    std::vector<Symbol*> Table;
+    int last_var_offset;
+
+    SymbolTable();
+
+    void insertSymbol(const Symbol &symbol);
+    bool isSymbolInTable(const std::string &name);
+
+
+    ~SymbolTable() = default;
+};
+
+
+class SymbolTableStack{
+
+    std::vector<SymbolTable*> symbol_table_stack;
+    std::vector<int> offset_stack;
+
+    void insertSymbol(std::string name, std::string type,int offset,bool is_symbol_function,std::vector<std::string> functions_params);
+
+    void make_table();
+
+    SymbolTableStack();
+
+    ~SymbolTableStack() = default;
+
+
+
+};
+
+
+
+
 
 
 #endif //COMPI_HW3_SYMBOTABLE_H
